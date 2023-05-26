97c84165cc2e537a7212bf7e8aa8d6e2796e8c79
AlexVienne <alexvienne@yandex.com>
2023-05-26 20:13:27 +0300

    FilterUpdate

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,27 +1,32 @@
 import './app-filter.css';
 
-const AppFilter = () => {
+const AppFilter = (props) => {
+
+    const buttonsData = [
+        {name: 'all', lable: 'Все сотрудники'},
+        {name: 'rise', lable: 'На повышение'},
+        {name: 'moreThan1000$', lable: 'З/П больше 1000$'}
+    ];
+
+    const buttons = buttonsData.map(({name, lable}) => {
+        const active = props.filter === name;
+        const clazz = active ? 'btn btn-light' : 'btn btn-outline-light';
+        return (
+            <button className={clazz}
+                    type="button"
+                    key={name}
+                    onClick={() => props.onFilterSelect(name)}
+            >
+                {lable}
+            </button>
+        )
+    })
+
     return (
         <div className="btn-group">
-            <button className="btn btn-light"
-                    type="button"
-            >
-                Все сотрудники
-            </button>
-
-            <button className="btn btn-outline-light"
-                    type="button"
-            >
-                На повышение
-            </button>
-
-            <button className="btn btn-outline-light"
-                    type="button"
-            >
-                З/П больше 1000$
-            </button>
+            {buttons}
         </div>
-    );
+    )
 }
 
 export default AppFilter;
