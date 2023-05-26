1c9448912e450323d3d0051dea93ceb556af9af0
AlexVienne <alexvienne@yandex.com>
2023-05-26 17:54:31 +0300

    SearchPanelReleased

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,12 +1,30 @@
 import './search-panel.css';
+import { Component } from 'react';
 
-const SerachPanel = () => {
-    return (
-        <input type="text"
+class SerachPanel extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            term: ''
+        }
+    }
+
+    onUpdateSearch = (e) => {
+        const term = e.target.value;
+        this.setState({term});
+        this.props.onUpdateSearch(term);
+    }
+
+    render () {
+        return (
+            <input type="text"
                className="form-control search-input"
                placeholder="Найти сотрудника" 
-        />
-    );
+               value={this.state.term}
+               onChange={this.onUpdateSearch}
+            />
+        )
+    }
 }
 
 export default SerachPanel;
